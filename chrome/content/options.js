0e934893404c301272d2d272ec809665b8df0966
voccs <ryan@voccs.com>
2006-03-02 22:15:51 +0000

    extension preferences
    git-svn-id: file:///opt/svn/confirmfmv/src@6 2c568b65-b8c1-4502-b95c-6df1030576df

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,1 +1,22 @@
-// options
+var ConfirmfmvOptions = {}
+
+ConfirmfmvOptions.onLoad = function() {
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    var confirmFolderDrag = prefs.getBoolPref("folders.drag.confirm");
+    var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
+    if (confirmFolderDrag)
+        checkbox.setAttribute("checked", "true");
+}
+
+ConfirmfmvOptions.onUnload = function() {
+    // do nothing
+}
+
+ConfirmfmvOptions.save = function() {
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
+
+    prefs.setBoolPref("folders.drag.confirm", checkbox.checked);
+
+    window.close();
+}
