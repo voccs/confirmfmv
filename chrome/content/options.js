092559228312aa5bbb9ebdfe99ea9b00f4e80e2d
voccs <ryan@voccs.com>
2009-12-06 12:39:27 +0000

    bsd license, changing options to use prefwindow
    git-svn-id: file:///opt/svn/confirmfmv/src@21 2c568b65-b8c1-4502-b95c-6df1030576df

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,22 +1,5 @@
 var ConfirmfmvOptions = {}
 
-ConfirmfmvOptions.onLoad = function() {
-    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-    var confirmFolderDrag = prefs.getBoolPref("folders.drag.confirm");
-    var disableDrag = prefs.getBoolPref("folders.drag.disable-dialog");
-    var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
-    var checkbox_disable = document.getElementById("checkbox_confirmfmvDisableDialog");
-    if (confirmFolderDrag)
-        checkbox.setAttribute("checked", "true");
-    if (disableDrag)
-        checkbox_disable.setAttribute("checked", "true");
-    ConfirmfmvOptions.setup();
-}
-
-ConfirmfmvOptions.onUnload = function() {
-    // do nothing
-}
-
 ConfirmfmvOptions.setup = function() {
     var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
     var checkbox_disable = document.getElementById("checkbox_confirmfmvDisableDialog");
@@ -24,16 +7,4 @@ ConfirmfmvOptions.setup = function() {
         checkbox_disable.removeAttribute("disabled");
     else
         checkbox_disable.setAttribute("disabled", "true");
-}
-
-ConfirmfmvOptions.save = function() {
-    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-    var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
-    var checkbox_disable = document.getElementById("checkbox_confirmfmvDisableDialog");
-
-    prefs.setBoolPref("folders.drag.confirm", checkbox.checked);
-    if (checkbox.checked)
-        prefs.setBoolPref("folders.drag.disable-dialog", checkbox_disable.checked);
-
-    window.close();
-}
+};
