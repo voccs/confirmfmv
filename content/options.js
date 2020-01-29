dd4d0dec6df8a6a47bb34ca11fac3dbc937ae3f9
voccs <ryan@voccs.com>
2020-01-29 02:14:20 -0800

    potentially correct but no preferences button shown

--- content/options.js
+++ content/options.js
@@ -1,10 +1,16 @@
+Preferences.addAll([
+  { id: "extensions.confirmfmv.drag.confirm", type: "bool" },
+  { id: "extensions.confirmfmv.drag.disableDialog", type: "bool" },
+]);
+
 var ConfirmfmvOptions = {}
 
 ConfirmfmvOptions.setup = function() {
-    var checkbox = document.getElementById("checkbox_confirmfmvSwitch");
-    var checkbox_disable = document.getElementById("checkbox_confirmfmvDisableDialog");
-    if (checkbox.checked)
-        checkbox_disable.removeAttribute("disabled");
-    else
-        checkbox_disable.setAttribute("disabled", "true");
+  let checkbox = document.getElementById("checkbox_confirmfmvSwitch");
+  let checkbox_disable = document.getElementById("checkbox_confirmfmvDisableDialog");
+  if (checkbox.checked) {
+    checkbox_disable.removeAttribute("disabled");
+  } else {
+    checkbox_disable.setAttribute("disabled", "true");
+  }
 };
